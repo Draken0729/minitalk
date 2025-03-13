2598c4ec2d13de49e0bf7b28564bec2b950e217f
Quentin Baret <qbaret@c1r1p2.42nice.fr>
2025-03-13 17:15:18 +0100

    fini

--- client.c
+++ client.c
@@ -3,38 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: quentin <quentin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: qbaret <qbaret@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 17:05:23 by quentin           #+#    #+#             */
-/*   Updated: 2025/03/12 16:18:12 by quentin          ###   ########.fr       */
+/*   Updated: 2025/03/13 17:08:46 by qbaret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void send_bits(pid_t server_pid, char c)
+void send_bits(int server_pid, char c)
 {
     int i;
     
     i = 0;
     while (i < 8)
     {
-        if ((c >> i) & 1)
-            kill(server_pid, SIGUSR2);
-        else
+        if (c & (1 << (7 - i)))
             kill(server_pid, SIGUSR1);
+        else
+            kill(server_pid, SIGUSR2);
 
         usleep(350);
         i++;
     }
 }
 
-void send_message(pid_t server_pid, char *message)
+void send_message(int server_pid, char *msg)
 {
-    int i = 0;
-    while (message[i])
+    int i;
+
+    i = 0;
+    while (msg[i])
     {
-        send_bits(server_pid, message[i]);
+        send_bits(server_pid, msg[i]);
         i++;
     }
     send_bits(server_pid, '\0');
@@ -43,16 +45,22 @@ void send_message(pid_t server_pid, char *message)
 int main(int argc, char **argv)
 {
 
-    if (argc != 3)
+    if (argc < 3)
 	{
-        ft_printf("Usage: %s <PID> <Message>\n", argv[0]);
-        return 1;
+        ft_printf("Error\n");
+        return (0);
     }
+    else
+    { 
+        int server_pid = atoi(argv[1]);
+        if (server_pid <= 0)
+        {
+            ft_printf("invalid PID.\n");
+            return 1;
+        }
+        char *message = argv[2];
 
-    pid_t server_pid = atoi(argv[1]);
-
-    ft_printf("Envoi du message : \"%s\" au PID %d\n", argv[2], server_pid);
-    send_message(server_pid, argv[2]);
-
+        send_message(server_pid, message);
+    }
     return 0;
 }
--- server.c
+++ server.c
@@ -3,38 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: quentin <quentin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: qbaret <qbaret@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 18:09:05 by rabril-h          #+#    #+#             */
-/*   Updated: 2025/03/12 16:12:27 by quentin          ###   ########.fr       */
+/*   Updated: 2025/03/13 17:09:13 by qbaret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void signal_handler(int sig, siginfo_t *info, void *context)
+void signal_handler(int sig)
 {
     static char c = 0;
     static int bit_index = 0;
 
-    (void)context;
-
-    if (sig == SIGUSR2)
-        c |= (1 << bit_index);
-
+    if (sig == SIGUSR1)
+        c |= (1 << (7 - bit_index));
     bit_index++;
-
+    
     if (bit_index == 8)
     {
-        write(1, &c, 1);
-        
         if (c == '\0')
             write(1, "\n", 1);
-
+        else
+            write(1, &c, 1);
         bit_index = 0;
         c = 0;
-
-        kill(info->si_pid, SIGUSR1);
+    }
+    if (sig == SIGINT)
+    {
+        ft_printf("\nserver closed.\n");
+        _exit(0);
     }
 }
 
@@ -43,21 +42,19 @@ int main(void)
     struct sigaction sa;
     
     sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = signal_handler;
+    sa.sa_handler = signal_handler;
     sigemptyset(&sa.sa_mask);
-
-    ft_printf("Serveur en attente... PID: %d\n", getpid());
 
     if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
     {
-        perror("sigaction");
-        return 1;
+        ft_printf("Error\n");
+        return (0);
     }
-
+    
+    signal(SIGINT, signal_handler);
+    ft_printf("Server waiting... PID: %d\n", getpid());
     while (1)
         pause();
-
-    return 0;
 }
 
 
